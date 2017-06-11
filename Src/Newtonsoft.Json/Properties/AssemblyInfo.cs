9951e6abc8841c89c8536e62b710100e3eabc303
Leandro Cannizzaro <leandro.cannizzaro@gmail.com>
2017-06-11 22:10:06 +0200

    Added doG Key

--- Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
+++ Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
@@ -29,6 +29,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
+[assembly: AssemblyKeyFileAttribute(@"c:\doG\Projects\doGApplications\doG\doG.snk")]
+
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -58,8 +61,8 @@ using System.Security;
 
 #if !SIGNED
 
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Schema")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Tests")]
+//[assembly: InternalsVisibleTo("Newtonsoft.Json.Schema")]
+//[assembly: InternalsVisibleTo("Newtonsoft.Json.Tests")]
 #else
 [assembly: InternalsVisibleTo("Newtonsoft.Json.Schema, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
 [assembly: InternalsVisibleTo("Newtonsoft.Json.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
